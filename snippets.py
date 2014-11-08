69e711e94bec37ef9a42d97f9d73df87238eb24b
vishwanath79@outlook.com <vishwanath79@outlook.com>
2014-11-07 22:05:09 -0800

    save snippets,accept arguments

--- snippets.py
+++ snippets.py
@@ -22,6 +22,16 @@ def make_parser():
 	logging.info("Constructing parser")
 	description = "Store and retrieve snippets of text"
 	parser = argparse.ArgumentParser(description=description)
+	
+	subparsers = parser.add_subparsers(dest="command",help="Available commands")
+	# Subparser for put command
+	logging.debug("Constructing put subparser")
+	put_parser = subparsers.add_parser("put",help="Store a snippet")
+	put_parser.add_argument("name", help = "the name of the snippet")
+	put_parser.add_argument("snippet", help = "snippet text")
+	put_parser.add_argument("filename", default = "snippets.csv",nargs="?", help = "the snippet filename")
+	
+	
 	return parser
 	
 def main():
@@ -29,6 +39,13 @@ def main():
 	logging.info("Starting snippets")
 	parser=make_parser()
 	arguments = parser.parse_args(sys.argv[1:])
+	#Convert parsed arguments from Namespace to dictionary
+	arguments = vars(arguments)
+	command = arguments.pop("command")
+	
+	if command == "put":
+		name,snippet = put(**arguments)
+		print "Stored {!r} as {!r}".format(snippet,name)
 
 if __name__ == "__main__":
 	main()
